ea555af95197ca9336aa8724a9b8216ae90d42e1
Rizki Wanda Putra <rizkiwandaputra@gmail.comm>
2023-02-27 18:02:56 +0700

    laravel view

--- routes/web.php
+++ routes/web.php
@@ -172,7 +172,7 @@ Route::get('/', function () {
 // });
 
 // Route::get('/mahasiswa',function(){
-//     $arrMahasiswa = ["Dony salmanan","Acung","dilan","indra kenz"];
+//     $arrMahasiswa = ["dony salmanan","Acung","dilan","indra kenz"];
 //     return view ('kampus.mahasiswa')->with('mahasiswa',$arrMahasiswa); 
 // });
 
